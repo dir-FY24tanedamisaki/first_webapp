55965b0be9d5281ba52ae96fd6c2c7744a46d116
misaki.taneda <misaki.taneda.j847@dirbato.co.jp>
2024-04-22 12:48:26 +0900

    Lesson16①

--- first_webapp/src/test/GetServlet.java
+++ first_webapp/src/test/GetServlet.java
@@ -8,27 +8,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class GetServlet
- */
 @WebServlet("/GetServlet")
 public class GetServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public GetServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.setCharacterEncoding("UTF-8");
+        //request.setCharacterEncoding("UTF-8");
 
         String q = request.getParameter("q");
 
